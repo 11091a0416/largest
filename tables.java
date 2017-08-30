4dbea833afcfd37bed2431d545b373597a37c68e
Chakrapani Reddy <chakri1258@gmail.com>
2017-08-30 16:38:39 -0400

    new another file added

---
+++ tables.java
@@ -0,0 +1,15 @@
+
+public class tables {
+
+	public static void main(String[] args) {
+		int i,n=5,res;
+		System.out.println("table of five is");
+		for(i=1;i<=10;i++)
+		{
+		res=n*i;
+		
+		System.out.println(+res);
+		}
+	}
+
+}
